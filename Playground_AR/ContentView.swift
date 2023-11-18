44ffdd6803f43982ed15cb0bd2121986847fb18f
Settawatg <settawat.bpalm@gmail.com>
2023-11-18 17:28:59 +0700

    Initial Commit

---
+++ Playground_AR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Playground_AR
+//
+//  Created by Kisses MJ on 18/11/2566 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Playground_AR/Playground_ARApp.swift
@@ -0,0 +1,17 @@
+//
+//  Playground_ARApp.swift
+//  Playground_AR
+//
+//  Created by Kisses MJ on 18/11/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct Playground_ARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
