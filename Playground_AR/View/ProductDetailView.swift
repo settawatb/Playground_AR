21b68f5759d49236993aaf16f6fe7a2d17e9cc07
settawatb <settawat.bpalm@gmail.com>
2023-12-25 19:52:44 +0700

    Building Product Detail Page

---
+++ Playground_AR/View/ProductDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductDetailView.swift
+//  Playground_AR
+//
+//  Created by Kisses MJ on 25/12/2566 BE.
+//
+
+import SwiftUI
+
+struct ProductDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductDetailView()
+}
---
+++ Playground_AR/ViewModel/SharedDataModel.swift
@@ -0,0 +1,18 @@
+//
+//  SharedDataModel.swift
+//  Playground_AR
+//
+//  Created by Kisses MJ on 25/12/2566 BE.
+//
+
+import SwiftUI
+
+struct SharedDataModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SharedDataModel()
+}
