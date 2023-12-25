f07856a0f6887c0eabe401e6a61934e000c47d1b
settawatb <settawat.bpalm@gmail.com>
2023-12-25 21:40:20 +0700

    Build Favorite Page

---
+++ Playground_AR/View/TabViews/FavoritePage.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritePage.swift
+//  Playground_AR
+//
+//  Created by Kisses MJ on 25/12/2566 BE.
+//
+
+import SwiftUI
+
+struct FavoritePage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoritePage()
+}
