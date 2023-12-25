7d56ab6c59efaf2a8b0fa01d4e252a0f2864f87a
settawatb <settawat.bpalm@gmail.com>
2023-12-26 00:17:53 +0700

    Build Cart Page

---
+++ Playground_AR/View/TabViews/CartPage.swift
@@ -0,0 +1,18 @@
+//
+//  CartPage.swift
+//  Playground_AR
+//
+//  Created by Kisses MJ on 25/12/2566 BE.
+//
+
+import SwiftUI
+
+struct CartPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CartPage()
+}
