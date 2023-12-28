f748352f6e411b68bca0933538bbf580e337f12d
settawatb <settawat.bpalm@gmail.com>
2023-12-29 01:15:07 +0700

    App + AR Mode

---
+++ Playground_AR/ARMode/FocusARView.swift
@@ -0,0 +1,18 @@
+//
+//  FocusARView.swift
+//  Playground_AR
+//
+//  Created by Kisses MJ on 29/12/2566 BE.
+//
+
+import SwiftUI
+
+struct FocusARView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FocusARView()
+}
