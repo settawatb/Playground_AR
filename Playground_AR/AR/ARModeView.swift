e22fa75ce2578968e5c516da4586fb729f3612dc
settawatb <settawat.bpalm@gmail.com>
2023-12-29 01:24:14 +0700

    Add ARView and sample AR Model

---
+++ Playground_AR/AR/ARModeView.swift
@@ -0,0 +1,18 @@
+//
+//  ARModeView.swift
+//  Playground_AR
+//
+//  Created by Kisses MJ on 29/12/2566 BE.
+//
+
+import SwiftUI
+
+struct ARModeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ARModeView()
+}
---
+++ Playground_AR/AR/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Playground_AR
+//
+//  Created by Kisses MJ on 29/12/2566 BE.
+//
+
+import SwiftUI
+
+struct AppDelegate: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppDelegate()
+}
